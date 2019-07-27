a26d1f25a06fe4be55a17d4d19410048998a6d44
imanipaul <imanipaul@wustl.edu>
2019-07-27 11:46:08 -0400

    Replace FlatList with SectionList

--- BookList.js
+++ BookList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SectionList, SafeAreaView } from 'react-native';
 
 import BookItem from './BookItem'
 import NYT from './NYT'
@@ -10,7 +10,8 @@ class BookList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            // data: []
+            sections: []
         };
     }
 
@@ -34,17 +35,56 @@ class BookList extends Component {
         });
     };
 
+    // _refreshData = () => {
+    //     NYT.fetchBooks().then(books => {
+    //         this.setState({ data: this._addKeysToBooks(books) })
+    //     });
+    // };
+
     _refreshData = () => {
-        NYT.fetchBooks().then(books => {
-            this.setState({ data: this._addKeysToBooks(books) })
-        });
+        Promise
+            .all([
+                NYT.fetchBooks('hardcover-fiction'),
+                NYT.fetchBooks('hardcover-nonfiction')
+            ])
+            .then(results => {
+                if (results.length !== 2) {
+                    console.error('Unexpected results');
+                }
+
+                this.setState({
+                    sections: [
+                        {
+                            title: "Hardcover Fiction",
+                            data: this._addKeysToBooks(results[0])
+                        },
+                        {
+                            title: "Hardcover NonFiction",
+                            data: this._addKeysToBooks(results[1])
+                        }
+                    ]
+                });
+            });
+    };
+
+    _renderHeader = ({ section }) => {
+        return (
+            <Text style={styles.headingText}>
+                {section.title}
+            </Text>
+        );
     };
 
     render() {
         return (
             <SafeAreaView style={styles.safearea}>
                 <View style={styles.container}>
-                    <FlatList data={this.state.data} renderItem={this._renderItem} />
+                    <SectionList
+                        sections={this.state.sections}
+                        renderItem={this._renderItem}
+                        renderSectionHeader={this._renderHeader}
+
+                    />
                 </View>
             </SafeAreaView>
         );
@@ -59,6 +99,17 @@ const styles = StyleSheet.create({
     safearea: {
         flex: 1,
         backgroundColor: 'white'
+    },
+    headingText: {
+        fontSize: 24,
+        alignSelf: 'center',
+        backgroundColor: 'lightblue',
+        fontWeight: 'bold',
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingTop: 2,
+        paddingBottom: 2,
+        width: '100%'
     }
 });
 
